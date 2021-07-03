92954b7c7a09308a766918df1a0ef04adb00a887
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-07-03 18:16:50 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+//Array of objects
+const quiz = [
+
+    {
+        q:"I watch ______ while I am sitting on the sofa.",
+        options:["television", "radio", "picture", "MP3 player"],
+        answer: 0
+    },
+    {
+        q:"I sometimes listen to the ______ .",
+        options:["television", "radio", "film", "type"],
+        answer: 1
+    },
+    {
+        q:"I keep my books in a ______ .",
+        options:["carpet", "armchair", "bookcase", "shelter"],
+        answer: 2
+    },
+    {
+        q:"We stand in a bus ______ to wait for a bus.",
+        options:["corner", "road", "station", "stop"],
+        answer: 3
+    },
+    {
+        q:"The cars wait until the traffic ______ turn green.",
+        options:["lights", "crossing", "signs", "railings"],
+        answer: 0
+    },
+    {
+        q:"You can cross the road at a ______ .",
+        options:["cross-road", "crossing", "crosser", "road"],
+        answer: 1
+    },
+    {
+        q:"We should put waste paper in a ______ .",
+        options:["box", "park", "litter-bin", "pavement"],
+        answer: 2
+    },
+    {
+        q:"We get wet when it ______ .",
+        options:["freezes", "blows", "shines", "rains"],
+        answer: 3
+    },
+    {
+        q:"When it is very cold, everything ______ .",
+        options:["freezes", "rains", "blows", "snows"],
+        answer: 0
+    },
+    {
+        q:"Children enjoy playing games when it ______ .",
+        options:["blows", "snows", "freezes", "is cold"],
+        answer: 1
+    }
+
+
+
+]
